c664ad2c06312ba957e1d27a8c73d6b4d903e061
MouleeswaranR <ffmouleesh@gmail.com>
2025-03-20 21:17:43 +0530

    small changes

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,38 @@
-import express from 'express';
-import authRoutes from './routes/authRoute.js';
-import movieRoutes from './routes/movieRoute.js';
-import tvRoutes from './routes/tvRoute.js';
-import searchRoutes from './routes/searchRoutes.js';
-import { ENV_VARS } from './config/envVars.js';
-import { connectDb } from './config/db.js';
-import cookieParser from 'cookie-parser';
-import { protectRoute } from './middleware/protectRoute.js';
+import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
 
+import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
 
-const app=express();
-const PORT=ENV_VARS.PORT;
-app.use(express.json());
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
+import { protectRoute } from "./middleware/protectRoute.js";
+
+const app = express();
+
+const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json()); // will allow us to parse req.body
 app.use(cookieParser());
-app.use("/api/v1/auth",authRoutes);
-app.use("/api/v1/movies",protectRoute,movieRoutes);
-app.use("/api/v1/tv",protectRoute,tvRoutes);
-app.use("/api/v1/search",protectRoute,searchRoutes);
-app.listen(PORT,()=>{
-    connectDb();
-    console.log("Server is listening to 5000");  
-})
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
+
+if (ENV_VARS.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+	console.log("Server started at http://localhost:" + PORT);
+	connectDB();
+});
